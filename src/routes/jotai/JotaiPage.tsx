81c12ff24c9c20c919735da5fb1161d32fa8288c
PeterWang <wang.t.peter@gmail.com>
2025-06-01 19:18:07 -0700

    React Router Dom has now been implemented.

---
+++ src/routes/jotai/JotaiPage.tsx
@@ -0,0 +1,18 @@
+export default function Jotai() {
+  return (
+    <>
+      <div>
+        <h1>Jotai Example</h1>
+        <p>This is a simple example of using Jotai for state management.</p>
+      </div>
+      <div className="card">
+        <p>
+          Edit <code>src/routes/Jotai.tsx</code> to modify this component.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the links to learn more about Jotai.
+      </p>
+    </>
+  );
+}
